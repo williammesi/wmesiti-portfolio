9489c003a21bff4949905b7db4761674e578119f
William <2246747@cstj.qc.ca>
2025-09-23 13:47:18 -0400

    Implemented Base ProjectDetail with sanity
    TODO : Improve ProjectDetail layout
    TODO : Optimize Sanity implementation

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,18 +1,20 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D2CZTxTC.mjs';
-import { manifest } from './manifest_BeyqgKfK.mjs';
+import { manifest } from './manifest_D_9bxPRF.mjs';
 
 const serverIslandMap = new Map();;
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/admin/_---params_.astro.mjs');
 const _page2 = () => import('./pages/projectdetail.astro.mjs');
-const _page3 = () => import('./pages/index.astro.mjs');
+const _page3 = () => import('./pages/projects/_slug_.astro.mjs');
+const _page4 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["node_modules/@sanity/astro/dist/studio/studio-route.astro", _page1],
     ["src/pages/ProjectDetail.astro", _page2],
-    ["src/pages/index.astro", _page3]
+    ["src/pages/projects/[slug].astro", _page3],
+    ["src/pages/index.astro", _page4]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -23,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "5f1b5180-c456-4106-a8b7-0f1685343945",
+    "middlewareSecret": "3e805b5c-330f-4e39-b8ac-db6aca7c19ff",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
