31c013be4fa722f96de4855478089e27c95ad2b3
William <2246747@cstj.qc.ca>
2025-09-22 20:36:11 -0400

    Implemented base projects component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,23 @@ import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [vue(), icon(), react()],
+  integrations: [
+    vue(),
+    icon(),
+    react(),
+    sanity({
+      projectId: "wmesiti-portfolio-cms",
+      dataset: "wmesiti-portfolio-data",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+    }),
+  ],
   // Removed webcore() since the package doesn't exist
 });
