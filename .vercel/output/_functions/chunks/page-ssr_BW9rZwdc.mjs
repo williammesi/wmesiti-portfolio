7891b43426ef95e1c7d97e25b4107380e2fc87af
William <2246747@cstj.qc.ca>
2025-09-23 20:08:24 -0400

    Remove .vercel folder to force fresh build

--- .vercel/output/_functions/chunks/page-ssr_BW9rZwdc.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"v2023-08-24","projectId":"9bdbnjzy","dataset":"production","useCdn":false}
-          );
-
-globalThis.sanityClient = sanityClient;
--- .vercel/output/static/_astro/studio-component.CHKMkR6d.js
+++
@@ -1,1 +0,0 @@
-import{b9 as p}from"./studio-component.CEgZqfX4.js";import"./client.DX9NJ8SR.js";export{p as StudioComponent};
